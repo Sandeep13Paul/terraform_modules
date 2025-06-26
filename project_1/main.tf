b3440132d2a577527ba132892c028ff620aae2ef
sonupaul13 <sonupaul213@gmail.com>
2025-06-26 12:39:24 +0530

    added

--- project_1/main.tf
+++ project_1/main.tf
@@ -1,7 +1,7 @@
 module "vpc_network" {
   source = "../modules/vpc_network"
-
-  networks = var.services.vpc_network.enabled ? var.services.vpc_network.networks : []
+  count = var.services["service_account"].enabled ? 1 : 0
+  networks = var.services.vpc_network.networks
 
   project_id = var.project_id
   region     = var.region
@@ -31,7 +31,8 @@ module "vm_instance" {
 
 module "service_accounts" {
   source = "../modules/service_accounts"
-  service_accounts = var.services.service_account.enabled ? var.services.service_account.service_accounts : []
+  count = var.services["service_account"].enabled ? 1 : 0
+  service_accounts = var.services["service_account"].service_accounts
 
   project_id = var.project_id
   region     = var.region
