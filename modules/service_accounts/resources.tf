70fa39f4bc6f0cf00326b737a71f2aa10515c2f1
sonupaul13 <sonupaul213@gmail.com>
2025-06-26 12:31:43 +0530

    added

--- modules/service_accounts/resources.tf
+++ modules/service_accounts/resources.tf
@@ -1,6 +1,6 @@
 resource "google_service_account" "service_account" {
   count = length(var.service_accounts)
-  display_name = "Service-Account-${random_id.suffix.hex}"
+  display_name = var.service_accounts[count.index].display_name
   account_id = var.service_accounts[count.index].account_id
   description = var.service_accounts[count.index].description
   project = var.service_accounts[count.index].project_id
