8bca19a399bfddcf82ac273d32e928f719f16133
sonupaul13 <sonupaul213@gmail.com>
2025-07-02 11:33:01 +0530

    added changes

--- modules/vm_instance/resources.tf
+++ modules/vm_instance/resources.tf
@@ -5,7 +5,7 @@ resource "google_compute_instance" "vm_sandeep_tf" {
   machine_type = each.value.machine_type
   zone         = each.value.zone
   can_ip_forward = false
-  tags           = ["http-server", "https-server"]
+  tags           = ["http-server", "https-server", "allow-ssh"]
 
   boot_disk {
     initialize_params {
