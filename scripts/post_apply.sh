d6fc1a6f67d53355f1b3e95d3f9f29e01982393e
sonupaul13 <sonupaul213@gmail.com>
2025-07-03 11:22:51 +0530

    added changes

--- scripts/post_apply.sh
+++ scripts/post_apply.sh
@@ -1,11 +1,39 @@
+# #!/bin/bash
+
+# echo "[Atlantis] Running post-apply script..."
+
+# cd project_1
+
+# echo "[Atlantis] Fetching VM IPs from Terraform output..."
+# terraform output -json vm_public_ips > vm_ips.json
+
+# cd ../ansible
+
+# rm -f inventory.txt
+
+# echo "[web]" > inventory.txt
+
+# for ip in $(jq -r '.[]' /vm_ips.json); do
+#   echo "$ip ansible_ssh_private_key_file='../../../ssh_key'" >> inventory.txt
+# done
+
+# echo "[Atlantis] Generated inventory:"
+# cat inventory.txt
+
+# echo "[Atlantis] Running Ansible playbook..."
+# ansible-playbook -i inventory.txt main.yml
+# rm /tmp/temp_gcp_key
+
 #!/bin/bash
+set -e
+set -x
 
 echo "[Atlantis] Running post-apply script..."
 
 cd project_1
 
 echo "[Atlantis] Fetching VM IPs from Terraform output..."
-terraform output -json vm_public_ips > vm_ips.json
+terraform output -json vm_public_ips > ../vm_ips.json
 
 cd ../ansible
 
@@ -13,13 +41,12 @@ rm -f inventory.txt
 
 echo "[web]" > inventory.txt
 
-for ip in $(jq -r '.[]' /vm_ips.json); do
-  echo "$ip ansible_ssh_private_key_file='../../../ssh_key'" >> inventory.txt
+for ip in $(jq -r '.[]' ../vm_ips.json); do
+  echo "$ip ansible_user=ubuntu ansible_ssh_private_key_file=../ssh_key" >> inventory.txt
 done
 
 echo "[Atlantis] Generated inventory:"
 cat inventory.txt
 
 echo "[Atlantis] Running Ansible playbook..."
-ansible-playbook -i inventory.txt playbook.yml
-rm /tmp/temp_gcp_key
+ansible-playbook -i inventory.txt main.yml
