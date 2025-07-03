45ec8ee7caf817b3447a4d3383c474af819bff58
sonupaul13 <sonupaul213@gmail.com>
2025-07-03 11:56:38 +0530

    added changes

--- scripts/post_apply.sh
+++ scripts/post_apply.sh
@@ -45,6 +45,7 @@ rm -f inventory.txt
 echo "[web]" > inventory.txt
 
 for ip in $(jq -r '.[]' ../vm_ips.json); do
+  ssh-keygen -R "$ip" || true
   echo "$ip ansible_user=ubuntu ansible_ssh_private_key_file=../ssh_key" >> inventory.txt
 done
 
@@ -52,4 +53,5 @@ echo "[Atlantis] Generated inventory:"
 cat inventory.txt
 
 echo "[Atlantis] Running Ansible playbook..."
+ssh-keygen -R 34.142.238.164
 ansible-playbook -i inventory.txt main.yml
