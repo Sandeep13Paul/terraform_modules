a3212bb117e49eed2f2cbad5e0eabe339f0a3938
sonupaul13 <sonupaul213@gmail.com>
2025-07-03 11:02:01 +0530

    added changes

--- scripts/post_apply.sh
+++ scripts/post_apply.sh
@@ -13,11 +13,8 @@ rm -f inventory.txt
 
 echo "[web]" > inventory.txt
 
-echo "$PRIVATE_KEY_CONTENTS" > /tmp/temp_gcp_key
-chmod 600 /tmp/temp_gcp_key
-
 for ip in $(jq -r '.[]' /vm_ips.json); do
-  echo "$ip ansible_ssh_private_key_file='/tmp/temp_gcp_key'" >> inventory.txt
+  echo "$ip ansible_ssh_private_key_file='./ssh_key'" >> inventory.txt
 done
 
 echo "[Atlantis] Generated inventory:"
